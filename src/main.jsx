e39725e59802fbcbd806097eb5ec5abd5cd2f3f5
onetwothird <angelitodecatoriaa@gmail.com>
2026-04-27 14:05:59 +0800

    main initialization

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// IMPORTANT: Put your Client ID here or in your .env file
+const GOOGLE_CLIENT_ID = "985531066364-5hck5sakhct0j2keqkcdb2hca28mt260.apps.googleusercontent.com"; 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>,
 )
